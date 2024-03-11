df31b629eb9f66d0c1459dbc3916479f37230f28
Madiba Hudson-Quansah <mhquansah@gmail.com>
2024-03-11 18:23:31 +0000

    Added production constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export const HOST = "0.0.0.0";
 export const PORT = 3000;
 
-export const PG_USER = "mads";
-export const PG_PASS = "";
-export const PG_HOST = "0.0.0.0";
-export const PG_DB = "ashesi";
+let PG_USER: string;
+let PG_PASS: string;
+let PG_HOST: string;
+let PG_DB: string;
 export const PG_PORT = 5432;
+
+switch (process.env.NODE_ENV) {
+    case "dev":
+        PG_USER = "mads";
+        PG_PASS = "";
+        PG_HOST = "0.0.0.0";
+        PG_DB = "ashesi";
+        break;
+    case "prod":
+        PG_USER = process.env.PG_USER!;
+        PG_PASS = process.env.PG_PASSWORD!;
+        PG_HOST = process.env.PG_HOST!;
+        PG_DB = process.env.PG_DATABASE!;
+        break;
+}
+
+export { PG_DB, PG_PASS, PG_USER, PG_HOST };
